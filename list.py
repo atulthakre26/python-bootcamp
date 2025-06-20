9779b0813ac7a4b64fa80f4349335176fb85290e
Atul <thare.atul26@gmail.com>
2025-06-20 17:40:48 +0530

    Add list file

---
+++ list.py
@@ -0,0 +1,15 @@
+lst = [1,2,3,4,5,6,7,8,9]
+
+# Slicing
+"""
+
+Slicing[Start : End : Step]
+
+"""
+
+"""Q1. Get the only [5,6]"""
+print(lst[4:6])
+print()
+
+print(lst[2:6:2])
+
---
+++ loops.py
@@ -0,0 +1,10 @@
+x = 1
+while x <=50:
+    print("Hello Atul", x)
+    x +=1
+
+name = "Atul Thakre"
+
+for x in name:
+    print(x.upper())
+
